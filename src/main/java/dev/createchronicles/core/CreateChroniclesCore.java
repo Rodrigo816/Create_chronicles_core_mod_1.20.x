a2dfb645d49191c235b2682a3a6dbdcef28a3932
Rodrigo Borba <rodrigo14.84@gmail.com>
2024-09-18 19:21:01 +0000

    should work now with create-H

--- src/main/java/dev/createchronicles/core/CreateChroniclesCore.java
+++ src/main/java/dev/createchronicles/core/CreateChroniclesCore.java
@@ -1,5 +1,4 @@
 package dev.createchronicles.core;
-import com.mojang.logging.LogUtils;
 import com.yungnickyoung.minecraft.ribbits.entity.trade.ItemListing;
 import com.yungnickyoung.minecraft.ribbits.entity.trade.ItemsForAmethysts;
 import com.yungnickyoung.minecraft.ribbits.module.RibbitProfessionModule;
@@ -12,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+
 
 @Mod(CreateChroniclesCore.MOD_ID)
 public class CreateChroniclesCore {
@@ -27,18 +27,18 @@ public class CreateChroniclesCore {
     private void commonSetup(final FMLCommonSetupEvent event) {
 
         RibbitTradeModule.TRADES.put(RibbitProfessionModule.MERCHANT, new ItemListing[]{
-                new ItemsForAmethysts(getQuarkItem("quark:indigo_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:violet_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:white_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:black_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:yellow_corundum_cluster"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:indigo_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:violet_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:white_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:black_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:yellow_corundum"), 64, 64, 2, 2, 16),
         });
         RibbitTradeModule.TRADES.put(RibbitProfessionModule.FISHERMAN, new ItemListing[]{
-                new ItemsForAmethysts(getQuarkItem("quark:red_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:orange_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:yellow_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:green_corundum_cluster"), 64, 64, 2, 2, 16),
-                new ItemsForAmethysts(getQuarkItem("quark:blue_corundum_cluster"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:red_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:orange_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:yellow_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:green_corundum"), 64, 64, 2, 2, 16),
+                new ItemsForAmethysts(getQuarkItem("quark:blue_corundum"), 64, 64, 2, 2, 16),
         });
     }
 
